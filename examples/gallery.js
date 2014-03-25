cc6f93fcb039051eed4264be751b50297f4aebab
Jouni Santara <Jouni Santara@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-25 06:11:52 +0000

    Docs and examples in ...
    git-svn-id: https://plugins.svn.wordpress.org/nvd3-visualisations/trunk@881439 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ examples/gallery.js
@@ -0,0 +1,23 @@
+
+function nvd3Demos(pcs) { // pcs = nro of html container where chart appears
+
+jsChart(pcs, '', 'linePlusBar', {height:'200', width:'350'}, { showLegend: true, tooltips: true, transitionDuration: 3000, useInteractiveGuideline: true, xtime: true });
+pcs--;
+jsChart(pcs, '', 'simpleline', {height:'250', width:'300'}, { showLegend: false, transitionDuration:3000});
+pcs--;
+jsChart(pcs, '', 'scatterbubble', {height:'250', width:'300'}, { showLegend: true });
+pcs--;
+jsChart(pcs, '', 'viewfinder', {height:'250', width:'300'}, { showLegend: true });
+pcs--;
+jsChart(pcs, '', 'multibar', {height:'250', width:'300'}, { showLegend: false });
+pcs--;
+jsChart(pcs, '', 'cumulativeline', {height:'250', width:'300'}, { showLegend: false });
+pcs--;
+jsChart(pcs, '', 'stackedarea', {height:'250', width:'300'}, { showLegend: false });
+pcs--;
+jsChart(pcs, '', 'discretebar', {height:'250', width:'300'}, { color: ['red','green','blue','orange','brown','navy','yellow','black'] });
+pcs--;
+jsChart(pcs, '', 'horizontalmultibar', {height:'250', width:'400'}, { showLegend: false });
+pcs--;
+jsChart(pcs, '', 'pie', {height:'250', width:'300'}, { showLegend: true });
+}
