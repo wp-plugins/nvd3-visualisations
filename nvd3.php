93bcf99eed23cd32a23ba5e9bb9c6915a8a132e6
Jouni Santara <Jouni Santara@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-02 04:58:12 +0000

    Data sets cloning.
    git-svn-id: https://plugins.svn.wordpress.org/nvd3-visualisations/trunk@906680 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nvd3.php
+++ nvd3.php
@@ -1,9 +1,9 @@
 <?php
-/*
+/* 
 Plugin Name: NVD3 Visualisations
 Plugin URI: http://wordpress.org/extend/plugins/d3-simplecharts/
 Description: Draw business class interactive charts from any data set of files or own custom functions.
-Version: 1.3.0
+Version: 1.3.1
 Author: Jouni Santara
 Organisation: TERE-tech ltd 
 Author URI: http://www.linkedin.com/in/santara
