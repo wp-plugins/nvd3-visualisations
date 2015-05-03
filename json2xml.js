b8776782b674715c18d742f55a6314c00e1c2d5b
Jouni Santara <Jouni Santara@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-03 06:28:33 +0000

    return of json2xml.js back to normal
    git-svn-id: https://plugins.svn.wordpress.org/nvd3-visualisations/trunk@1152102 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- json2xml.js
+++ json2xml.js
@@ -1,375 +1,110 @@
-<?php
-/* 
-Plugin Name: NVD3 Visualisations
-Plugin URI: http://wordpress.org/extend/plugins/d3-simplecharts/
-Description: Draw business class interactive charts from any data set of files or own custom functions.
-Version: 1.8.2
-Author: Jouni Santara
-Organisation: TERE-tech ltd 
-Author URI: http://www.linkedin.com/in/santara
-License: GPL2 
+/*	This work is licensed under Creative Commons GNU LGPL License.
+
+	License: http://creativecommons.org/licenses/LGPL/2.1/
+   Version: 1.0
+	Author:  Stefan Goessner/2006, Jouni Santara/2014
+	Web:     http://goessner.net/ 
 */
+function json2xml(o, tab) {
+   var toXml = function(v, name, ind) {
+      recdepth++;
 
-// All included ext.files
-function write_headers($rood_dir) {
-	echo '<!-- Start of NVD3 -->'; 
-
-	$root = plugins_url().'/'.$rood_dir.'/'; 	// Single, MU-site, and SSL setups of WP
-												// http://codex.wordpress.org/Function_Reference/plugins_url
-
-	// D3.js lib
-	// echo '<script src="'.$root.'d3.v3.1.5.js"></script>'; // Uncomment if problems with new d3 v3.4.6 below
-	echo '<script src="'.$root.'d3.min.js"></script>';
-	// NVD3.js lib
-	echo '<link href="'.$root.'nv.d3.css" rel="stylesheet" type="text/css">'; 
-
-	echo '<script src="'.$root.'nv.d3.min.js"></script>';  // Comment this line if you need to develope NVD3 core lib
-	// echo '<script src="'.$root.'nv.d3.js"></script>'; 		// + activate & edit this source file + minimize to above file
-
-	echo '<script src="'.$root.'xml2json.js"></script>';  // Used for XML data sets reading
-	echo '<script src="'.$root.'json2xml.js"></script>';  // Used for JSON -> XML conversions
-	echo '<script src="'.$root.'tsv2json.js"></script>';  // Used for TSV -> JSON converting tool
-
-	echo '<script src="'.$root.'colorbrewer.js"></script>'; // Predefined pretty coloring sets
-
-	// NVD3 Visualisations main routines
-	echo '<script src="'.$root.'locale.js"></script>';
-	echo '<script src="'.$root.'wpcharts.js"></script>';
-
-	echo '<!-- End of NVD3 -->';
-}
-// Uncomment & write headers part of *every* blog page (=> no need to [loadNVD3])
-// add_action('wp_head', 'write_headers');
-// Note: to make it faster ext.files not included on page/post with no charts.
-
-function myroot() { // Finding root directory for JavaScript
-	$rood_dir='nvd3-visualisations';
-	write_headers($rood_dir);
-	setRootDir($rood_dir);
-}
-add_shortcode("rootDir", "myroot");
-add_shortcode("loadNVD3", "myroot");
-
-function setRootDir($rood_dir) {  
-?>
-<script>
-	rootpath = '<?php echo plugins_url().'/'.$rood_dir.'/data/' ?>';
-</script>
-<?php
-	return plugins_url() . '/'.$rood_dir.'/';
-}
-
-function templatePicker($data) {
-
-	myroot();
-
-	// Libs for UX
-	echo '<link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">';
-	echo '<script src="//code.jquery.com/jquery-1.10.2.js"></script>';
-	echo '<script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>';
-
-	$rood_dir='nvd3-visualisations';
-	$root = setRootDir($rood_dir);
-	echo '<script src="'.$root.'examples/gallery.js"></script>';
-
-//	$buildTabs = ' jQuery(function() { jQuery( "#tabs" ).tabs(); }); ';
-//	echo '<div id="tabs"></div>';
-//	echo '<script>'.$buildTabs.' jQuery( "#tabs" ).html( nvd3Charter() ); </script>';
-	echo '<span id="mycharts"></span>';
-	echo '<script> nvd3Charter("mycharts"); </script>';
-}
-add_shortcode("NVD3Picker", "templatePicker");
-add_shortcode("demosGallery", "templatePicker");
-
-// Nice demo gallery for the most of NVD3 chart types - START
-function demoCharts($data) {
-
-	$rood_dir='nvd3-visualisations';
-	write_headers($rood_dir);
-	$skeleton = demoContainers();
-	$count = $skeleton["count"]; // How many demos to show 
-	$root = setRootDir($rood_dir);
-	// $root = 'wp-content/plugins/nvd3/';
-	echo '<script src="'.$root.'examples/gallery.js"></script>';
-
-	// $count = 11;
-	if ($data['count'])	$count = $data['count'];
-	$xmldemo = 0;
-	if ($data['xmldemo'] == 'true' || $data['xmldemo'] == 1)	$xmldemo = 1;
-
-	return $skeleton["places"] . '<script> nvd3Demos('.$count.', '.$xmldemo.'); </script>';
-}
-add_shortcode("demosGallery_old", "demoCharts");
-
-function demoContainers() { 
-
-	
-	$html = '<style type="text/css" media="screen">';
-	$html .= '.demo_nvd3 {vertical-align:bottom;} ';
-	$html .= '.odds_nvd3 {background-color:#FFEBCD;} ';
-	$html .= '.title_nvd3 {color: navy; text-shadow:2px 2px darkgray;} ';
-	$html .= '</style>';
-
-	$html .= '<table><tbody><tr class="demo_nvd3">';
-	$html .= '<td id="chart1" class="odds_nvd3"></td>';
-	$html .= '<td id="chart2"></td>';
-	$html .= '</tr><tr class="demo_nvd3">';
-	$html .= '<td id="chart3"></td>';
-	$html .= '<td id="chart4" class="odds_nvd3"></td>';
-	$html .= '</tr><tr class="demo_nvd3">';
-	$html .= '<td id="chart5" class="odds_nvd3"></td>';
-	$html .= '<td id="chart6"></td>';
-	$html .= '</tr><tr class="demo_nvd3">';
-	$html .= '<td id="chart7"></td>';
-	$html .= '<td id="chart8" class="odds_nvd3"></td>';
-	$html .= '</tr><tr class="demo_nvd3">';
-	$html .= '<td id="chart9" class="odds_nvd3"></td>';
-	$html .= '<td id="chart10"></td>';
-	$html .= '</tr><tr class="demo_nvd3">';
-	$html .= '<td id="chart11"></td>';
-	$html .= '<td id="chart12" class="odds_nvd3"></td>';
-	$html .= '</tr></tbody></table>';
-
-	// echo $html;
-
-	return array("count"=>12, "places"=>$html); // Count of cells / demos
-}
-// Demos - END
-
-function filterIn($x, $name, $quotes) { 
-
-	$x = trim($x);
-	$sep = ",";
-	if (strpos($x, "\t") > -1)	// tab => ","
-		$x = str_replace("\t", $sep, $x);
-	if (strpos($x, ";") > -1)  // ";" => ","
-		$x = str_replace(";", $sep, $x);
-	if ($quotes && strpos($x, "'") === false && strpos($x, '"') === false) // Automatic quotes: "x","y" ...
-		$x = '"' . str_replace($sep, '"'.$sep.'"', $x) . '"';
-	$x = str_replace("(", " ", $x);
-	$x = str_replace(")" ," ", $x);
-	
-	// Build valid JS array ready
-	$x = ' '.$name.':[' . $x . '] ';
-	return $x;
+	  var xml = "";
+	  if (v instanceof Array) {
+         if (!inarray)  // Inside 1st array recursion - remember it
+			arraylevel = recdepth;
+		 inarray = true;
+		 xstack.push(new Array("##A##",v));
+		 for (var i=0, n=v.length; i<n; i++)
+            { xml += ind + toXml(v[i], name, ind+"\t") + "\n";
+			 }
+		 if (name == "values" && inarray) { // Standard notation for array elements
+			if (recdepth > arraylevel)
+				xml = "\n<element>\n" + xml + "\n</element>"; // Down under top
+			else
+				xml = "\n<values>\n" + xml + "\n</values>";  // Top level cell
+		} else
+			 xml = "\n<" + name + ">\n" + xml + "\n</" + name + ">"; // Other type cells
+		
+		if (arraylevel == recdepth) // Out of array's all recursions
+			inarray = false;
+      }
+      else if (typeof(v) == "object") {
+		 xstack.push(new Array("##O##",v));
+         if (name == "values")
+			xml = toXml(v, "element", "\t");
+		 else {
+		 var hasChild = false;
+			xml += ind + "<" + name;
+         for (var m in v) {
+            if (m.charAt(0) == "@")
+               xml += " " + m.substr(1) + "=\"" + v[m].toString() + "\"";
+            else
+               hasChild = true;
+         }
+         xml += hasChild ? ">" : "/>";
+         if (hasChild) {
+            for (var m in v) {
+               if (m == "#text")
+                  xml += v[m];
+               else if (m == "#cdata")
+                  xml += "<![CDATA[" + v[m] + "]]>";
+               else if (m.charAt(0) != "@")
+                  xml += toXml(v[m], m, ind+"\t");
+            }
+            xml += (xml.charAt(xml.length-1)=="\n"?ind:"") + "\t</" + name + ">";
+         }
+		 }
+      }
+      else {
+		 // xstack.push(new Array("##X##",v));
+         if (inarray && name == "values")
+			xml = ind + "<element>" + v.toString() + "</element>";
+		 else
+			xml += ind + "<" + name + ">" + v.toString() +  "</" + name + ">";
+      }
+	  recdepth--;
+      return xml;
+   }, 
+   xml="";
+   var inarray = false;
+   var recdepth = 0;
+   var arraylevel = 0;
+   var xstack = new Array();
+   for (var m in o)
+      xml += toXml(o[m], m, "");
+   // console.info(xstack);
+   return tab ? xml.replace(/\t/g, tab) : xml.replace(/\t|\n/g, "");
 }
 
-// API for the shortcode [jsChart] on WP
-function newChart($data) { 
+// Main function to convert JSON obj -> XML obj / string
+function json22xml(data, tab, juststring) {
 
-	$id = mt_rand(10,10000);
-	if ($data['id']) // User's own defined ID of container
-		$id = $data['id'];
+		// Create tree of xml with its root
+		var cells = '';
+		if (!data.length)
+			cells = cells + '<element>\n' + json2xml(data,tab) + '\n</element>';
+		else
+		for (i=0; i<data.length; i++)
+			cells = cells + '<element>\n' + json2xml(data[i],tab) + '\n</element>';
+		cells = '<root>\n' + cells + '\n</root>';
 
-	$ctype = 'simpleline';  // Def. chart's type
-	if ($data['type'])
-		$ctype = $data['type'];
+	if (juststring)
+		return cells;
 
-// *** Direct input values turns to options of input data
-	$values = '';
-	if ($data['values']) {		// Input format: (1,2,3, ...) OR TODO: ((1,2,3),(11,22,33), ...)
-		$infile = 'foo'; 		// special flag of direct simple input set
-		$values = filterIn($data['values'], 'values', false);
-		if ($data['labels']) {
-			$values = $values . ', ' .filterIn($data['labels'], 'labels', true);
+	// console.info(cells);
+	// xmlDoc = 0; // debug: change this global
+	// Convert txt tags into XML object
+	if (window.DOMParser)
+		{
+		parser=new DOMParser();
+		xmlDoc=parser.parseFromString(cells,"text/xml");
 		}
-		if ($data['series']) {
-			$values = $values . ', ' .filterIn($data['series'], 'series', true);
+	else // Internet Explorer
+		{
+		xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+		xmlDoc.async=false;
+		xmlDoc.loadXML(cells); 
 		}
-		if ($data['links'])
-			$values = $values . ', ' .filterIn($data['links'], 'links', true);
-	} else if ($data['class'])  { // Direct data coming from class (& ID's) of tags
-		$values = ' class:"' . $data['class'] . '" ';
-		if (strpos($data['class'],'id:') > 0) // Object input: {id:"mytable", bgcolor:"blue"}
-			$values = ' class:' . $data['class'] . ' ';
-		$infile = 'foo'; // special flag of direct simple input
-	} else if ($data['table'])  { // Direct data coming from  table
-		$values = ' table:"' . $data['table'] . '" ';
-		$infile = 'foo'; // special flag of direct simple input
-	}
-
-	$options = '';  // Def. options for charts
-	if ($data['options']) {
-		$options = ', ###'.trim($data['options']);
-		if ($values != '')
-			$values = $values.',';
-		$options = str_replace('###{', '{'.$values.' ', $options);
-	} else if ($infile = 'foo' && $values)
-		$options = ', { ' . $values . ' }';
-// ***
-
-  // Input data file name / rel.path
-	if ($data['datafile']) {
-		$infile = $data['datafile'];
-		$options = str_replace('}', ', infile:"'.$infile.'" }', $options);
-	}
-
-	$container = 'div'; // Def.type of container
-	if ($data['container']) // User's choice
-		$container = $data['container'];
-
-	$height = '250'; // Def.height of chart (must exist)
-	if ($data['height']) // User's choice
-		$height = $data['height'];
-	$width = ' width:450';  // Def. width
-	if ($data['width'])
-		$width = ' width:'.$data['width'];
-
-	$bgcolor = '';  // Back color
-	if ($data['backgroundcolor']) // background-color
-		$bgcolor = ' background-color:'.$data['backgroundcolor']. ';';
-	$border = '';  // Border style around
-	if ($data['border'])
-		$border = ' border:'.$data['border']. ';'; 
-
-	$float = ' float:none; ';  
-	if ($data['float']) // Embed on right/left
-	if ($data['float'] == 'right' || $data['float'] == 'right') {
-		$float = ' float:'.$data['float']. ';';
-		$container = 'span';  // Best element for container
-		$border = ''; // Visually ugly with span tag
-	}
-
-	$margin = '';  // Margins around charts
-	if ($data['margin'])
-		$margin = ' margin:'.$data['margin'].'; '; 
-
-	$jsfunc = 0;  // Case of data's generating JavaScript
-	if ($data['js'])
-		$jsfunc = 1;
-
-//	$resize = ' resize:both; overflow:auto; ';
-	$html = '<'.$container.' id="chart'.$id.'" style="'.$float.$bgcolor.$border.$margin.' ">';
-	$html .= "<svg style='height:".$height."px;".$width."px;'/>";
-	$html .= "</".$container.">"; 
-
-	if ($jsfunc == 0)  // File name to ext.data file
-	  	$infile = "'" . $infile . "'";
-
-
-	$js = "jsChart('".$id."', ".$infile.", '".$ctype."', {height:'".$height."', ".$width."} ".$options." );";
-
-	$jsCall = "<script>".$js."</script>";
-
-	return $html . $jsCall;
+	return xmlDoc;
 }
-add_shortcode("jsChart", "newChart");
-/*
-// Generata JSON variable from direct data input
-function buildTSV($data) {
-
-	
-	$values = $data['values'];
-	$values = str_replace('(', '', $values);
-	$values = str_replace(')', '', $values);
-	$values = explode(',',$data['values']);
-	$labels = 0;
-	if ($data['labels'])
-		$labels = explode(',',$data['labels']);
-	// TODO: generate TSV temp file and return its name back
-
-	return '';
-}
-*/
-// Print out data editor for a given data set
-function genEditor($data) {
-
-if (get_post_status() == 'publish')
-	return; // Hide editor from public
-
-$owndata = $data["infile"];
-// $chart = $data["type"];
-
-$ctype = $data["type"];
-
-$chartdata = file_get_contents($owndata);
-
-$jscall = "saveData('xmlheader', 'dataset', '".$owndata."')";
-$json2xml = "dataConvert('json', 'jsonset', 'xmlset', '".$ctype."')";
-$xml2json = "dataConvert('xml', 'xmlset', 'jsonset', '".$ctype."')";
-$tsv2json = "dataConvert('tsv', 'jsonset', 'xmlset', '".$ctype."')";
-$csv2json = "dataConvert('csv', 'jsonset', 'xmlset', '".$ctype."')";
-$json2tsv = "json2tsv('jsonset')";
-
-$syntax = '';
-$syntaxedit = '';
-$converter = '';
-$convertbox = '';
-$jsondata = '';
-$xmldata = '';
-$header = '';
-if (strpos($owndata,'.json') || strpos($owndata,'.tsv') || strpos($owndata,'.csv')) {
-	$syntaxedit = '<div class="nvd3_editor">Edit your data set here and check syntax of JSON.
-<iframe name="(c) 2013 Jos de Jong" src="http://www.jsoneditoronline.org/" width="100%" height="500"></iframe></div>';
-	$syntax='JSON Editor';
-	
-	$converter = 'Data Converter';
-	$jsondata = $chartdata; 
-}
-if (strpos($owndata,'.xml')) {
-	$header = explode("<root>", $chartdata);
-	$chartdata = "<root>" . $header[1];
-	$header = $header[0];
-
-	$syntaxedit = '<div class="nvd3_editor">Edit your data set here and check syntax of XML.
-<iframe src="http://www.freeformatter.com/xml-formatter.html" width="100%" height="500"></iframe></div>';
-	$syntax='XML Editor';
-
-	$converter = 'Data Converter</sup>';
-	$xmldata = $chartdata;
-}
-$msg = '<br /><p>Here is your new chart created by NVD3 Visualisations: <b>edit, publish & enjoy it!</b></p> ';
-// $datalink = 'Data File: <a href="'.$owndata.'" target="_blank"><b>'.$owndata.'</b></a>';
-$datatitle = 'Data File: '.$owndata;
-
-// Return whole editor for a post/page
-return $msg.'<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
-<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-<br />
-
-  <script>
-  $(function() { $( "#tabs" ).tabs(); });
-  </script>
-
-<div id="tabs">
-  <ul>
-    <li><a href="#dataedit-1" title="'.$datatitle.'">Chart Data Set</a></li>
-    <li><a href="#dataedit-2">'.$syntax.'</a></li>
-    <li><a href="#dataedit-3">'.$converter.'</a></li>
-  </ul>
-
-  <div id="dataedit-1">
-<div class="nvd3_editor">
-<button onclick="'.$jscall.'" style="cursor:pointer">Save & Refresh Chart</button><br />
-<textarea id="dataset" class="nvd3_editor_text" cols="70" rows="240">
-'.$chartdata.'
-</textarea>
-<span id="xmlheader" style="display: none;">'.$header.'</span>
-</div>
-  </div>
-
-  <div id="dataedit-2">
-    '.$syntaxedit.'
-  </div>
-
-  <div id="dataedit-3">
-	<textarea id="jsonset" class="nvd3_editor_text" cols="40" rows="240">
-		'.$jsondata.'
-	</textarea>
-	<br />
-	<button onclick="'.$json2xml.'" style="cursor:pointer">JSON to XML</button>
-	<button onclick="'.$xml2json.'" style="cursor:pointer">XML to JSON</button>
-	<br />
-	<button onclick="'.$tsv2json.'" style="cursor:pointer" title="Data columns separated by TABs">TSV to JSON</button>
-	<button onclick="'.$csv2json.'" style="cursor:pointer" title="Data columns separated by ; or , letters">CSV to JSON</button>
-	<br />
-	<textarea id="xmlset" class="nvd3_editor_text" cols="40" rows="240">
-		'.$xmldata.'
-	</textarea>
-  </div>
-  
-</div>
-	';
-}
-add_shortcode("dataEditor", "genEditor");
-?>
